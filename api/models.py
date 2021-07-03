ee06b85a7ea527849853dfabd44b799981addead
Himanshu <himanshudhiman9313@gmail.com>
2021-07-03 15:57:14 +0530

    updated

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 from datetime import date
 from django.db import models
 from django.db.models.base import Model
-from django.db.models.fields import DurationField
+from django.contrib.auth.models import AbstractUser 
+
+
+
+
 
 class Licence(models.Model):
     license_no = models.EmailField(null=False, blank=False)
@@ -9,7 +13,7 @@ class Licence(models.Model):
     client_secret = models.TextField(null=False, blank=False, default="NA")
     installation_url = models.TextField(null=False, blank=False, default="NA")
     refresh_token = models.TextField(null=False, blank=False, default="NA")
-    count = models.IntegerField(null=False, blank=False)
+    count = models.IntegerField(null=False, blank=False, default=0)
 
     def __str__(self):
         return self.license_no
@@ -61,3 +65,47 @@ class Batch(models.Model):
 
     def __str__(self):
         return self.subject.name + " - " + self.teacher.first_name + " - " + self.start_date.strftime('%m/%d/%Y') + " to " + self.end_date.strftime('%m/%d/%Y')
+
+
+
+
+class User(AbstractUser):
+    public_name = models.CharField(max_length=100, null=True, blank=True)
+    user_type = models.CharField(max_length=100, null=True, blank=True)
+    phone_no = models.CharField(max_length=100, null=True, blank=True)
+    country_code = models.CharField(max_length=10, null=True, blank=True)
+    head_line = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    address = models.TextField(blank=True, null=True)
+    experience = models.TextField(blank=True, null=True)
+    academics = models.TextField(blank=True, null=True)
+    teaching_interests = models.TextField(blank=True, null=True)
+    # course_name = models.ManyToManyField(Course, blank=True)
+    batch = models.ManyToManyField(Batch, blank=True)
+    facebook_id = models.CharField(max_length=100, blank=True, null=True)
+    gmail_id = models.CharField(max_length=100, blank=True, null=True)
+    profile_pic = models.ImageField(
+        upload_to='profile_pic/', null=True, blank=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    facebook_pic = models.TextField(blank=True, null=True)
+    gmail_pic = models.TextField(blank=True, null=True)
+    input_text = models.TextField(blank=True, null=True)
+    resume = models.FileField(upload_to='resumes', null=True, blank=True)
+    teacher_intro = models.FileField(upload_to='intro_videos', null=True, blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    gender = models.CharField(max_length=100, blank=True, null=True)
+    hobbies = models.TextField(blank=True, null=True)
+    facebook_link = models.TextField(blank=True, null=True)
+    instagram_link = models.TextField(blank=True, null=True)
+    youtube_link = models.TextField(blank=True, null=True)
+    linkedin_link = models.TextField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    accepted_by_admin = models.BooleanField(default=False)
+    video_url = models.TextField(blank=True, null=True)
+    profile_pic_url = models.TextField(blank=True, null=True)
+    application_status = models.BooleanField(default=False)
+    number_verify_status = models.BooleanField(default=False)
+    rejected_by_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
